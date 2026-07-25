0e01fb4f1f0e115b2bca0795ab198ab70c4c150e
imforked <villegasdominick@gmail.com>
2026-07-24 17:17:41 -0700

    Install Motion

--- client/src/components/Login/components/Drawer/Drawer.tsx
+++ client/src/components/Login/components/Drawer/Drawer.tsx
@@ -1,15 +1,21 @@
 import { Button } from "../../../Button";
 // TODO: improve imports
 import { ButtonVariant } from "../../../Button/Button.types";
+import { motion } from "motion/react";
 
 export const Drawer = () => {
   return (
-    <div className="absolute bottom-0 left-[50%] translate-x-[-50%] backdrop-blur-sm h-[35svh] w-[90%] bg-background/70 rounded-md flex flex-col items-center px-5">
+    <motion.div
+      className="absolute bottom-0 left-[50%] translate-x-[-50%] backdrop-blur-sm h-[35svh] w-[90%] bg-background/70 rounded-md flex flex-col items-center px-5"
+      initial={{ bottom: "-500px" }}
+      animate={{ bottom: "0px", transition: { duration: 0.5 } }}
+    >
       <h2 className="w-fit font-serif text-5xl tracking-wide leading-none text-balance mt-13 text-center">
         Good Stuff
       </h2>
       <p className="w-fit text-md text-center">
-        Recommendations from people <span className="whitespace-nowrap">you trust.</span>
+        Recommendations from people{" "}
+        <span className="whitespace-nowrap">you trust.</span>
       </p>
 
       <Button
@@ -24,6 +30,6 @@ export const Drawer = () => {
         variant={ButtonVariant.SECONDARY}
         className="mt-3"
       />
-    </div>
+    </motion.div>
   );
 };
